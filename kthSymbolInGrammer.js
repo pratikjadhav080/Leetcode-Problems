47930aca35e699414592385760d6a1d1e31c771e
Pratik Jadhav <pratikjadhav080@gmail.com>
2021-10-16 01:21:18 +0530

    kth symbol using recursion

---
+++ kthSymbolInGrammer.js
@@ -0,0 +1,21 @@
+//https://leetcode.com/problems/k-th-symbol-in-grammar/
+
+/**
+ * @param {number} n
+ * @param {number} k
+ * @return {number}
+ */
+ var kthGrammar = function(n, k) {
+    
+    if(n===1 || k===1){
+       return 0
+   }
+   let mid = Math.pow(2,n-1)/2
+   
+   if(k<= mid)
+       return kthGrammar(n-1,k)
+   else{
+       return !kthGrammar(n-1,k-mid)
+   }
+   
+};
